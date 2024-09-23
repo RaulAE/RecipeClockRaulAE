af56c858a7967fd059a0731bfd6a896000f9da28
Raul Aquino Escobar <raquinoescobar@students.mchenry.edu>
2024-09-22 23:08:32 -0500

    Required files added

---
+++ ClockRecipeRaulAE.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// Ingredients (variables used)
+var second = 0
+var minute = 0
+var hour = 0
+
+// Loops that counts up seconds
+
+// 1. have a loop that increases the second variable by += 1 every time the value is checked
+//  1a. I Will do this by having a while loop increase the value of the second variable while it is below 60
+//  1b. When the second variable reaches 60 I will set the second variable to 0 and the minute variable to += 1
+// 2. if second reaches 60 the second variabel is reset to zero and the minute variable is increased by 1
+//  2a. I will do this by setting up a while statement to check if the minute variable is below 60
+//  2b. When the minute variable reaches 60 I will set the minute variable variable to 0 and the hour variable to += 1
+// 3. The minute variable reaches 60 it will be reset and the hour variable will increase by 1
+//  3a. The hour variable will only keep count of the passing time
